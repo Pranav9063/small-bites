39255578975fcb349153728b9799a32bc90fc59c
Pradneya <pradneya.prog@gmail.com>
2025-01-30 19:04:22 +0530

    Added Safe Routes

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-const SignUpPage = () => {
-  return (
-    <View>
-      <Text>SignUpPage</Text>
-    </View>
-  )
+export default function Page() {
+    const user = auth().currentUser;
+    const signOut = async () => {
+        try {
+            // Sign out from Firebase
+            await auth().signOut();
+
+            // Sign out from Google
+            await GoogleSignin.signOut();
+
+            alert('Sign out successful!');
+        } catch (error) {
+            console.error('Error signing out: ', error);
+            alert('Failed to sign out. Please try again.');
+        }
+    };
+
+    return (
+        <View>
+            <Text>Welcome {user?.displayName}</Text>
+            <Button title="Sign Out" onPress={signOut} />
+        </View>
+    );
 }
-
-export default SignUpPage
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,48 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { configureOptions } from "@/config/config";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
+
+  const [initialising, setInitialising] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const router = useRouter();
+  const segments = useSegments();
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    // console.log("onAuthStateChanged", user);
+    setUser(user);
+    if (initialising) setInitialising(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
   useEffect(() => {
     GoogleSignin.configure(configureOptions);
   }, []);
+
+  useEffect(() => {
+    if (initialising) return;
+
+    const inAuth = segments[0] === '(auth)';
+    console.log("inAuth", inAuth);
+    if (user && !inAuth) router.replace('/(auth)');
+    else if (!user && inAuth) router.replace('/');
+  }, [user, initialising]);
+
+  if (initialising) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  };
 
   return (
     <Stack>
