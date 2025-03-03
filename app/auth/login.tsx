a384f9edb6ef78bc3df2b5ed89dffad68ea0d568
Pranav9063 <pranav9063@gmail.com>
2025-03-03 20:30:51 +0530

    loginScreenV1

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text } from "react-native";
+import { View, Text, StyleSheet, Animated, Image } from "react-native";
 import GoogleSignInButton from "@/components/GoogleSignInButton";
 import { useAuth } from "@/lib/context/AuthContext";
 import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
-import { FC } from "react";
+import { Link} from "expo-router";
+import { FC, useEffect, useRef  } from "react";
 import { Theme } from "@/constants/Theme";
 
 const LoginPage: FC = () => {
@@ -13,16 +13,35 @@ const LoginPage: FC = () => {
 
   const styles = createStyles(theme);
 
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
+    <Animated.View style={[styles.innerContainer, { opacity: fadeAnim }]}>
+      <Image source={require("@/assets/images/icon.jpg")} style={styles.logo} />
+      <Text style={styles.heading}>Welcome to Small Bites 🍽️</Text>
+      <Text style={styles.subheading}>
+        Order your favorite food from campus canteens with ease!
+      </Text>
+
       <GoogleSignInButton onPress={signIn} />
+
       <Text style={styles.text}>
-        New User? Continue to{" "}
+        New User?{" "}
         <Link href="/auth/sign-up/user" style={styles.link}>
           Sign up
         </Link>
       </Text>
-    </SafeAreaView>
+    </Animated.View>
+  </SafeAreaView>
   );
 };
 
@@ -32,17 +51,43 @@ const createStyles = (theme: Theme) =>
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
-      backgroundColor: theme.colors.background,
+      backgroundColor: "#a1f0f7",
+      paddingHorizontal: 20,
+    },
+    innerContainer: {
+      alignItems: "center",
+      width: "100%",
+    },
+    logo: {
+      width: 100, 
+      height: 100,
+      marginBottom: 20,
+      borderRadius: 50,
+      resizeMode: "contain",
+    },
+    heading: {
+      fontSize: 26,
+      fontWeight: "bold",
+      color: theme.colors.primary,
+      marginBottom: 10,
+      textAlign: "center",
+    },
+    subheading: {
+      fontSize: 16,
+      color: theme.colors.onBackground,
+      marginBottom: 30,
+      textAlign: "center",
     },
     text: {
-      fontSize: 12,
-      fontWeight: "bold",
-      marginBottom: 20,
+      fontSize: 14,
+      fontWeight: "500",
+      marginTop: 20,
       color: theme.colors.onBackground,
     },
     link: {
       color: theme.colors.primary,
       textDecorationLine: "underline",
+      fontWeight: "bold",
     },
   });
 
