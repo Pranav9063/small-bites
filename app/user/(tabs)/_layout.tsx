ad5d8d6ce49aa93c859fc0051649a9d17f588d76
Pradneya <pradneya.prog@gmail.com>
2025-04-01 15:18:22 +0530

    Started custom tab bar

--- app/user/(tabs)/_layout.tsx
+++ app/user/(tabs)/_layout.tsx
@@ -1,38 +1,51 @@
-import { Tabs } from 'expo-router';
-import UserTabBar from '@/components/UserTabBar';
-import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
+import UserTabBar from "@/components/UserTabBar";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   return (
-    // tabBar={props => <UserTabBar {...props} />}
-    <Tabs  screenOptions={{ tabBarActiveTintColor: 'blue' , headerShown: false, }}>
+    <Tabs
+      tabBar={(props) => <UserTabBar {...props} />}
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
-          headerShown: false
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="home" color={color} />
+          ),
+          headerShown: false,
         }}
       />
       <Tabs.Screen
         name="favourites"
         options={{
-          title: 'Favourites',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="heart-outline" color={color} />,
+          title: "Favourites",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="heart-outline" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="expenses"
         options={{
-          title: 'Expenses',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="cash-outline" color={color} />,
+          title: "Expenses",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="cash-outline" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="person-outline" color={color} />,
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="person-outline" color={color} />
+          ),
         }}
       />
     </Tabs>
